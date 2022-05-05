0a869dfbcf8364b532d1ce326b77ccb05b5c3407
Joanna Katarzyna Klara <sc19jkk@leeds.ac.uk>
2022-05-05 16:28:07 +0100

    Finalt version

--- cmd_line.py
+++ cmd_line.py
@@ -8,15 +8,10 @@ import string
 from nltk.stem.porter import PorterStemmer
 import csv
 import pandas as pd
-import sys
-maxInt = sys.maxsize
-
 
 index = {}
-#urls = []
 dict_from_csv = {}
 new_index = {}
-
 
 
 def visible_text(element):
@@ -31,34 +26,30 @@ def visible_text(element):
 
 def read_url(url):
     ps = PorterStemmer()
-
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
     text = soup.findAll(text=True)
     result = list(filter(visible_text, text))
-    counter = 0;
     words = []
     for i in result:
         temp = i.split(' ')
         for word in temp:
             k = []
-            temp_word = word.lower()
-            for c in temp_word:
+            for c in word:
                 if c not in list(string.punctuation):
                     k.append(c)
-            temp_word = ''.join(k)
-            words.append(temp_word)
+            word = ''.join(k)
+            words.append(word)
     for i in words:
-        if (i.isalpha()):
+        if i.isalpha():
             i = ps.stem(i)
-            if not i in index.keys(): #or - if i not in index.keys():
-                index[i] = [url] #index[i] = [(url, counter)] #jeszcze sprawdź co to jest counter - znaczy,
-                counter = counter + len(i) + 1
+            if not i in index.keys():
+                index[i] = [url]
             else:
-                index[i].append(url) #index[i].append((url, counter))
-                counter = counter + len(i) + 1
+                index[i].append(url)
 
     return None
+
 
 
 #This command loads the index from the file system.
@@ -66,8 +57,6 @@ def read_url(url):
 #so, here i try loading index from file into a dictionary
 def load():
     dict_from_csv = pd.read_csv('scraping.csv').to_dict()
-    #print(dict_from_csv.keys()) #shows keys as Word, Frequency, List when dict_from_csv = pd.read_csv('scraping.csv').to_dict()
-    #print(dict_from_csv['Word'][0]) #this prints a
     words = []
     frequencies = []
     positing_lists = []
@@ -84,9 +73,6 @@ def load():
 
 
 while True:
-    # decrease the maxInt value by factor 10
-    # as long as the OverflowError occurs.
-
         command_line = input('Enter command to run: ')
         words = command_line.split() #take all the input string here divided into specific words
         command = words[0]
@@ -95,11 +81,11 @@ while True:
         crawler = Crawler(urls=['http://example.python-scraping.com/'])
 
         if command == "quit":
-            print('Exiting the program') #doesn't print stuff; quit works for breaking of the program but doesn't print stuff there
+            print('Exiting the program')
             exit(1)
 
         elif command == "build":
-            urls = crawler.run() #dla sprawdzenia, przed zaczęciem inverted index
+            urls = crawler.run()
             for url in urls:
                 if url != '#':
                     read_url(url)
@@ -109,6 +95,7 @@ while True:
                 fieldNames = ['Word', 'Frequency', 'Posting List']
                 csvWriter = csv.DictWriter(indexFile, fieldnames=fieldNames)  # create writeDirectory object
                 csvWriter.writeheader()  # writing the header
+                c = 0
                 for i in sorted_keys:
                     csvWriter.writerow({'Word': i, 'Frequency': len(index[i]), 'Posting List': index[i]})
 
--- print_and_find.py
+++ print_and_find.py
@@ -1,21 +1,9 @@
-from crawler import *
-import requests
-from bs4 import BeautifulSoup
-from bs4.element import Comment
-import re
-import string
-from nltk.stem.porter import PorterStemmer
-import csv
-import pandas as pd
 import collections
 
 #This command prints the inverted index for a particular word, for example:
 #print Peso
 #will print the inverted index for the word ‘Peso’
-#Note: ...should I just print stuff there directly by putting in the code, because otherwise we shadow the normal print
 def print_index(phrase, dictionary):
-    phrase = phrase.lower() #make sure the phrase is lowercase now, just as everything in index
-    #print(dictionary[phrase]) #używanie tego tylko działa
     for word_key in dictionary.keys():
         if word_key == phrase:
             print("Index for " + phrase + ": ")
@@ -27,7 +15,6 @@ def print_index(phrase, dictionary):
 #will return a list of all pages containing the word ‘Dinar’, while
 #find Area Afghanistan
 #will return all pages containing the words ‘Area’ and ‘Afghanistan’.
-#For simplicity assume that the search is case sensitive, so ‘Euro’ is not the same word as ‘euro’.
 def find(phrase, dictionary):
     all_got_urls = []
     x_list=[]
@@ -38,8 +25,8 @@ def find(phrase, dictionary):
         for key in dictionary.keys():
             if word==key:
                 list_indexed_tuples.append(dictionary[word])
-    #print(list_indexed_tuples[0][1])
-    #print(type(list_indexed_tuples[0][1])) #okay, czyli całość tutaj to string... nie wiem jak by to zamieniać,
+
+
     #gdyby chcieć porównywać pojedyńczo, trzeba by dla każdego url robić jedno porównanie słowa
     #tj gdy słowo jest w trzech urls, mamy 3 entries, bo to dla każdego to będzie coś
     for i in range(len(list_indexed_tuples)):
@@ -48,25 +35,12 @@ def find(phrase, dictionary):
         #print(x)
 
     for j in range(len(x_list)):
-        # split string by ,
-        #print(x_list[j])
         chunks = x_list[j].split(',')
         for ch in chunks:
             ch = ch.strip("[' ']")
-            #print(ch)
             all_got_urls.append(ch)
 
     print("Urls with the words: ")
-    #print out repeating urls
     print([item for item, count in collections.Counter(all_got_urls).items() if count > 1])
-
     #now that we have clear urls, we need to iterate over them and see if the url addresses repeat
-    #if they do, then store them in the final list, which will then be shown
-
-
-    #print(list_indexed_tuples[1]) #here we access the tuple
-    #print(list_indexed_tuples[1][1]) #here we're accessing the second part of the tuple i.e. the list of tuples with http and numbers
-    #print(list_indexed_tuples[1][1][1]) #but here...here we already have that that this is treaten as separate characters
-
-
 
